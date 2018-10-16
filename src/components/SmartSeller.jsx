5a9daf2d65f141c5855614e020cac131bca02d69
fikyair <shiming_xsm@163.com>
2018-10-16 10:59:20 +0800

    精明小卖家

---
+++ src/components/SmartSeller.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/index.less';
+
+class SmartSeller extends React.Component {
+
+    render(){
+        return(
+            <div>
+                在html页面中开发
+            </div>
+        )
+    }
+
+}
+
+export default SmartSeller;
